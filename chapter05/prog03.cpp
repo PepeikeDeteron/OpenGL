ea717db76900ccf90823d7ad5b159d4fe8f8c9bb
deteikepeperon@gmail.com <deteikepeperon@gmail.com>
2021-10-09 13:34:23 +0900

    図形を塗りつぶす

---
+++ chapter05/prog03.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <OpenGL/OpenGL.h>
+#include <GLUT/glut.h>
+
+// 図形を塗りつぶす
+void display(void) {
+  glClear(GL_COLOR_BUFFER_BIT);
+
+  glBegin(GL_POLYGON);
+  glColor3d(1, 0, 0);
+  glVertex2d(-0.9, -0.9);
+  glColor3d(0, 1, 0);
+  glVertex2d(0.9, -0.9);
+  glColor3d(0, 0, 1);
+  glVertex2d(0.9, 0.9);
+  glColor3d(1, 1, 0);
+  glVertex2d(-0.9, 0.9);
+  glEnd();
+
+  glFlush();
+}
+
+void init(void) {
+  glClearColor(0, 0, 1, 1);
+}
+
+int main(int argc, char *argv[]) {
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_RGBA);
+  glutCreateWindow(argv[0]);
+  glutDisplayFunc(display);
+  init();
+  glutMainLoop();
+
+  return 0;
+}
